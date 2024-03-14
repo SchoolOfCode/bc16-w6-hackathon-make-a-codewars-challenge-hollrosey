5eb966eb580fd8aff37293a5551c5600e06e8f94
Holly Massey <141242889+hollrosey@users.noreply.github.com>
2024-03-14 17:21:46 +0000

    skeleton added

---
+++ main.js
@@ -0,0 +1,18 @@
+// Create a cipher text variable
+const cipherText = "Let the mystery begin!";
+
+// Create a key object
+const key = {
+  letter: "letter",
+  letter: "letter",
+  letter: "letter",
+  letter: "letter",
+  letter: "letter",
+  letter: "letter",
+  letter: "letter",
+  letter: "letter",
+  letter: "letter",
+};
+
+// Define the function decodeCipher which accepts cipherText and key as parameters
+function decodeCipher(cipherText, key) {}
